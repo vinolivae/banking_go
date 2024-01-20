7b917f3b74f0f965100a9368bfb91243999afa71
Vinicius Oliveira <viniciusoa@outlook.com>
2024-01-19 22:41:00 -0300

    feat: create interface
    feat: create payment invoice feature
    
    chore: create examples in main function

--- main.go
+++ main.go
@@ -2,40 +2,98 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"go_projects/banking_go/accountGenerator"
 )
 
-type CurrentAccount struct {
-	holder        string
-	agencyNumber  int
-	accountNumber int
-	balance       float64
+/*
+Módulos que possuem a função "withdraw" que recebe um float64 e retorna uma string
+assina um contrato com essa interface.
+
+O uso dessa interface contempla as regras das contas que possuem a função withdraw.
+
+OBS..: As interfaces são implementadas de forma implicita. E o objetivo é reaproveitar
+métoros ou funções entre tipos diferentes.
+*/
+type VerifyAccount interface {
+	Withdraw(amount float64) (string, float64)
 }
 
 func main() {
-	viniciusCurrentAccount := CurrentAccount{holder: "vinicius", agencyNumber: 589, accountNumber: 123456, balance: 125.50}
+	joaoCurrentAccount := accountGenerator.CreateCurrentAccount("João")
+	joaoCurrentAccount.DepositAmount(100)
 
-	fmt.Println("Saldo da conta é", viniciusCurrentAccount.balance)
-	fmt.Println(viniciusCurrentAccount.withdraw(20))
-	fmt.Println("O saldo agora é", viniciusCurrentAccount.balance)
+	mariaCurrentAccount := accountGenerator.CreateCurrentAccount("Maria")
+	mariaCurrentAccount.DepositAmount(200)
+
+	luizSavingAccount := accountGenerator.CreateSavingAccount("Luiz")
+	luizSavingAccount.DepositAmount(300)
+
+	anaSavingAccount := accountGenerator.CreateSavingAccount("Ana")
+	anaSavingAccount.DepositAmount(400)
+
+	//210
+	//90
+	joaoCurrentAccount.Transfer(10, &mariaCurrentAccount)
+	fmt.Println("Saldo da Maria:", mariaCurrentAccount.GetBalance())
+	fmt.Println("Saldo do João:", joaoCurrentAccount.GetBalance())
+	fmt.Println()
+
+	//100
+	//200
+	mariaCurrentAccount.Transfer(10, &joaoCurrentAccount)
+	fmt.Println("Saldo do João:", joaoCurrentAccount.GetBalance())
+	fmt.Println("Saldo da Maria:", mariaCurrentAccount.GetBalance())
+	fmt.Println()
+
+	//90
+	joaoCurrentAccount.Withdraw(10)
+	fmt.Println("Saldo do João:", joaoCurrentAccount.GetBalance())
+	fmt.Println()
+
+	//190
+	mariaCurrentAccount.Withdraw(10)
+	fmt.Println("Saldo da Maria:", mariaCurrentAccount.GetBalance())
+	fmt.Println()
+
+	//290
+	luizSavingAccount.Withdraw(10)
+	fmt.Println("Saldo do Luiz:", luizSavingAccount.GetBalance())
+	fmt.Println()
+
+	//390
+	anaSavingAccount.Withdraw(10)
+	fmt.Println("Saldo da Ana:", anaSavingAccount.GetBalance())
+	fmt.Println()
+
+	//70
+	payInvoice(&joaoCurrentAccount, 20)
+	fmt.Println("Saldo do João:", joaoCurrentAccount.GetBalance())
+	fmt.Println()
+
+	// 160
+	payInvoice(&mariaCurrentAccount, 30)
+	fmt.Println("Saldo da Maria:", mariaCurrentAccount.GetBalance())
+	fmt.Println()
+
+	// 250
+	payInvoice(&luizSavingAccount, 40)
+	fmt.Println("Saldo do Luiz:", luizSavingAccount.GetBalance())
+	fmt.Println()
+
+	// 340
+	payInvoice(&anaSavingAccount, 50)
+	fmt.Println("Saldo da Ana:", anaSavingAccount.GetBalance())
+	fmt.Println()
 }
 
 /*
-A notação (c *CurrentAccount) define um método em uma estrutura.
-(c *CurrentAccount) define um receptor de método. "c" é uma variável que representa uma instância
-da estrutura "CurrentAccount" e "*CurrentAccount" significa que "c" é um ponteiro para uma
-instância de "CurrentAccount". Permitindo, assim, que a instância da estrutura seja modificada.
-*/
-func (c *CurrentAccount) withdraw(withdrawalAmount float64) string {
-	if validateWithdraw(c.balance, withdrawalAmount) {
-		c.balance -= withdrawalAmount
-		return "Saque de " + strconv.FormatFloat(withdrawalAmount, 'f', 2, 64) + " feito com sucesso!"
-	} else {
-		return "Saldo insuficiente!"
-	}
-}
+Para pagar boleto não precisamos identificar uma conta.
+A interface "verifyAccount" nos ajuda a executar essa ação para qualquer tipo de conta, contanto
+que ela possua a função "withdraw".
 
-func validateWithdraw(balance, withdrawalAmount float64) bool {
-	canWithdraw := balance >= withdrawalAmount && withdrawalAmount > 0
-	return canWithdraw
+A conta passada no parametro da função precisa conter o "&", assim o endereço será exposto para
+o ponteiro na função "withdraw". Ou definir um ponteiro aqui. e.g.: (v *VerifyAccount)
+*/
+func payInvoice(account VerifyAccount, invoiceAmount float64) {
+	account.Withdraw(invoiceAmount)
 }
